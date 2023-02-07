f8bd52542cd193f2b4b3f7b1db85bfa64d80d17a
vanshthakur9 <99399695+vanshthakur9@users.noreply.github.com>
2023-02-07 19:12:54 +0530

    Add files via upload

---
+++ areainsqfeet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int length,width,area1;
+    cout<<"enter dimensions"<<endl;
+    cin>>length>>width;
+    area1=length*width;
+     cout<<"area="<<area1<<endl;
+
+return 0;
+}
---
+++ arrays1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+   char vowels[] {'a','e','i','o','u'};
+   cout<< "the vowel is "<<vowels[0]<<endl;
+   cout<< "the vowel is "<<vowels[4]<<endl;
+   return 0;
+
+}
